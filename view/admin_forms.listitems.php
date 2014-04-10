839ea95f9d1e7bc5cb212946ceaed4d156a42196
dev <dev@dev.animalvsmachine.com>
2014-04-09 23:38:28 -0400

    'forms finished'

--- view/admin_forms.listitems.php
+++ view/admin_forms.listitems.php
@@ -1,7 +1,9 @@
-<h3>View / <?=$form['title'];?></h3>
-<ol>
-<?php foreach($list as $item)
-{ ?> 
-	<li><a href="%appurl%view/<?=$item['form_id'];?>/<?=$item['id'];?>/">Submitted on <?=date('F j, Y', strtotime($item['date']));?></a></li>
- <?php } ?>
-</ol>
+<div class="form_view">
+	<h3><?=$form['title'];?></h3>
+	<ol class="submissions">
+	<?php foreach($list as $item)
+	{ ?> 
+		<li><a href="%appurl%view/<?=$item['form_id'];?>/<?=$item['id'];?>/">Submitted on <?=date('F j, Y', strtotime($item['date']));?></a></li>
+	 <?php } ?>
+	</ol>
+</div>
--- view/admin_forms.view.php
+++ view/admin_forms.view.php
@@ -1,15 +1,14 @@
-<style type="text/css">
-	th { text-align: left; }
-</style>
-<h3>View / <?=$form['title'];?> / <?=date('F j, Y', strtotime($date));?></h3>
-<table width="500px">
-	<tr>
-		<th>Param</th>
-		<th>Value</th>
-	</tr>
+<div class="form_view">
+	<h3><?=$form['title'];?> / <?=date('F j, Y', strtotime($date));?></h3>
+	<table width="500px">
+		<tr>
+			<th>Parameter</th>
+			<th>Value</th>
+		</tr>
 
-<?php foreach($data as $key => $val)
-{ ?> 
-	<tr><td><?=$key;?></td><td><?=$val;?></td></tr>
- <?php } ?>
-</table> 
+	<?php foreach($data as $key => $val)
+	{ ?> 
+		<tr><td><?=$key;?></td><td><?=$val;?></td></tr>
+	 <?php } ?>
+	</table>
+</div>
