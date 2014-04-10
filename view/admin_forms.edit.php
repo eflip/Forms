8bf39dc37b0fa780347af708e2aff0a85ed5c7b7
dev <dev@dev.animalvsmachine.com>
2014-04-09 23:04:15 -0400

    'phase 3 complete'

--- view/admin_forms.edit.php
+++ view/admin_forms.edit.php
@@ -1,13 +1,19 @@
-<form action="%appurl%edit/<?=$form['id'];?>/" method="post" class="page-editor">
-	<input type="submit" value="Save" /> <br /><?=$msg;?>
-	Title: <input style="font-size: 22px; padding: 5px; width:100%" name="title" value="<?=htmlspecialchars($form['title'], ENT_QUOTES);?>" />
-	<br /><br />
-	Email: <input style="font-size: 22px; padding: 5px; width:100%" name="email" value="<?=$form['email'];?>" />
-	<br /><br />
-	Content: <textarea id="ckeditor" name="content" rows="25" style="width: 100%"><?=htmlspecialchars($form['content'], ENT_QUOTES);?></textarea>
-	<br />
-	[<a onclick="return confirm('Are you sure?');" href="%appurl%rm/<?=$form['id'];?>/">Delete</a>]
-	<br /><br />
-	<input type="submit" value="Save" /> <?=$msg;?>
+<form action="%appurl%edit/<?=$form['id'];?>/" method="post" class="form-editor">
+	<div class="save_form">
+		<input type="submit" value="Save Form" />
+	</div>
+	<div>
+		<?=$msg;?>
+	</div>
+	<div class="form_title">
+		<input style="font-size: 22px; padding: 5px; width:100%" name="title" value="<?=htmlspecialchars($form['title'], ENT_QUOTES);?>" />
+	</div>
+	<div class="form_email">
+		<input style="font-size: 22px; padding: 5px; width:100%" name="email" value="<?=$form['email'];?>" />
+	</div>
+	<textarea id="ckeditor" name="content" rows="25" style="width: 100%"><?=htmlspecialchars($form['content'], ENT_QUOTES);?></textarea>
+	<div class="delete">
+		<a onclick="return confirm('Are you sure?');" href="%appurl%rm/<?=$form['id'];?>/" class="delete_item">Delete</a>
+	</div>
 </form>
 <?php readfile(ROOT.'system/lib/editor.js'); ?> 
